d78cffcb61d82ecff35b5f1a40723ad704f86784
Adzwoolly <adamwoollen@gmail.com>
2020-05-08 14:12:13 +0100

    Setup initial project

---
+++ src/main/java/uk/adamwoollen/friendanimals/FriendAnimals.java
@@ -0,0 +1,17 @@
+package uk.adamwoollen.friendanimals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FriendAnimals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
